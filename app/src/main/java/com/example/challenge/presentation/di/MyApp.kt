0daacf2cc39bdb2cf0476040684cf9fda258bcc3
Amanda Soares <amanda.soares@ifood.com.br>
2024-05-23 19:11:03 -0300

    Iniciando op projeto e criando injecao de dependencia

---
+++ app/src/main/java/com/example/challenge/presentation/di/MyApp.kt
@@ -0,0 +1,18 @@
+package com.example.challenge.presentation.di
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class MyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Inicialize o Koin
+        startKoin {
+            // Fornecer o contexto Android
+            androidContext(this@MyApp)
+            // Incluir o módulo criado
+            modules(myModule)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/challenge/presentation/di/appModule.kt
@@ -0,0 +1,8 @@
+package com.example.challenge.presentation.di
+
+import org.koin.dsl.module
+
+
+val myModule = module {
+
+}
