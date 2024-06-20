e6e21e900166f1e5dd8be1e705f338239c276a15
Amanda Soares <amanda.soares@ifood.com.br>
2024-06-20 18:14:44 -0300

    Add main activity

--- app/src/main/java/com/example/challenge/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/challenge/presentation/view/MainActivity.kt
@@ -3,11 +3,18 @@ package com.example.challenge.presentation.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.ExperimentalMaterial3Api
+import com.example.challenge.R
 
-@OptIn(ExperimentalMaterial3Api::class)
-class MainActivity : AppCompatActivity() {
+
+class MainActivity :AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, VideoPlayerFragment())
+                .commit()
+        }
 
     }
 }
